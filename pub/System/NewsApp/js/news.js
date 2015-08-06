db8669b6a5e47639d0f43ffb43fcf9743825ae21
root <assessment@modell-aachen.de>
2015-08-06 16:15:00 +0200

    Sources from MahrNewsAppContrib, but generalized
    Release-Note: none
    
    Ticket: #9015

---
+++ pub/System/NewsApp/js/news.js
@@ -0,0 +1,84 @@
+jQuery(document).ready(function() {
+	
+	/**
+	* General Application Settings
+	**/	
+
+	//--- Set News Link
+	jQuery("#modacButtonsRIGHT").find('#modacSearchBox').before("<li id='toggleNews'><a href='#'>News</a></li>");
+
+	//--- Close Function for NewsSlider
+	jQuery('.rightBarClose').click(function() {
+        	jQuery('#toggleNews').trigger('click');
+	});
+
+	//--- Set function to open NewsSlider
+	jQuery('#toggleNews').toggle(function(){
+        	jQuery('#rightBarWrapper').show().animate({
+                	right : '0'
+        	}, 500);
+	}, function() {
+        	jQuery('#rightBarWrapper').animate({
+                	right : '-100%'
+        	}, 500, function() {
+                	jQuery(this).hide();
+        	});
+	});
+
+	/**
+	* WebRightBar
+	**/
+	jQuery('.modacRightBar .newsItem .summary').each(function() {
+        	var wholeText   = jQuery(this).text();
+           	var partText    = wholeText.substring(0, 300);
+           	if (wholeText.length > 300) {
+            		jQuery(this).after('<div class="partSummary">' + partText + ' ...</div>').hide();
+           	}
+ 	});
+
+        jQuery('.modacRightBar .newsItem .partSummary').unbind().bind('click', function() {
+        	jQuery(this).slideUp('normal', function() {
+         		jQuery(this).prev().slideDown('normal');
+          	});
+  	});
+
+        jQuery('.modacRightBar').find('img').each(function() {
+        	if (!(jQuery(this).parent().hasClass('bigThumb')) && !jQuery(this).parent().hasClass('newsTopIconLinks')) {
+        		jQuery(this).remove();
+       		}
+       	});
+	
+	/**
+	* Archive
+	**/
+	if (foswiki.preferences.TOPIC.indexOf('Archiv') >= 0) {
+		var rowNumber = 0;
+
+             	getNextArchivData(rowNumber);
+
+             	jQuery('.newsArchivItem').find('img').each(function() {
+             		if (!(jQuery(this).parent().hasClass('bigThumb'))) {
+             			jQuery(this).remove();
+             		}
+             	});
+
+          	jQuery(window).unbind('scroll').bind('scroll', function() {
+             		if(jQuery(window).scrollTop() + jQuery(window).height() == jQuery(document).height()) {
+             			rowNumber += 1;
+            			getNextArchivData(rowNumber);
+            		}
+          	});	
+	}
+});
+
+function getNextArchivData(rowNumber) {
+	jQuery.get(foswiki.preferences.SCRIPTURL + '/view' + foswiki.preferences.SCRIPTSUFFIX  + '/' + foswiki.preferences.WEB + '/ArchivContentLoad?contenttype=text/plain;skin=text;section=archivLazyLoading;rowNumber=' + rowNumber , function(data) {
+		jQuery('#archiveContentArea').append(data);
+  	}).done(function() {
+		jQuery('#archiveContentArea').find('img').each(function() {
+  			if (!(jQuery(this).parent().hasClass('bigThumb'))) {
+  				jQuery(this).remove();
+ 			}
+   		});
+  	});
+}
