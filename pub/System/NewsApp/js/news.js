7f5c01c54c429e811f6c429a94d80a6e12760c9c
root <assessment@modell-aachen.de>
2015-08-17 10:33:04 +0200

    Removed WebRightBar template. Instead load the Content dynamic with javascript (like the overview). Performance issue.
    Release-Note: none
    
    Ticket: #9015

--- pub/System/NewsApp/js/news.js
+++ pub/System/NewsApp/js/news.js
@@ -12,38 +12,17 @@ jQuery(document).ready(function() {
 	//--- Set function to open NewsSlider
 	jQuery('#toggleNews').toggle(function(){
         	jQuery('#rightBarWrapper').show().animate({
-                	right : '0'
+                	right : '0'	
         	}, 500);
+			getNextArchivData(rowNumber, jQuery('.modacRightBar').first(), 'rightBarContentLoad', setRightBarConfig);
 	}, function() {
         	jQuery('#rightBarWrapper').animate({
                 	right : '-100%'
         	}, 500, function() {
+					jQuery('.modacRightBar').first().empty();
                 	jQuery(this).hide();
         	});
-	});
-
-	/**
-	* WebRightBar
-	**/
-	jQuery('.modacRightBar .newsItem .summary').each(function() {
-        	var wholeText   = jQuery(this).html();
-           	var partText    = wholeText.substring(0, 300);
-           	if (wholeText.length > 300) {
-            		jQuery(this).after('<div class="partSummary">' + partText + ' ...</div>').hide();
-           	}
- 	});
-
-        jQuery('.modacRightBar .newsItem .partSummary').unbind().bind('click', function() {
-        	jQuery(this).slideUp('normal', function() {
-         		jQuery(this).prev().slideDown('normal');
-          	});
-  	});
-
-        jQuery('.modacRightBar').find('img').each(function() {
-        	if (!(jQuery(this).parent().hasClass('bigThumb')) && !jQuery(this).parent().hasClass('newsTopIconLinks')) {
-        		jQuery(this).remove();
-       		}
-       	});
+	});	
 	
 	/**
 	* Archive
@@ -51,31 +30,60 @@ jQuery(document).ready(function() {
 	if (foswiki.preferences.TOPIC.indexOf('WebHome') >= 0 && foswiki.preferences.WEB.indexOf('News') >= 0) {
 		var rowNumber = 0;
 
-             	getNextArchivData(rowNumber);
+        getNextArchivData(rowNumber, jQuery('#archiveContentArea'), 'archivLazyLoading');
 
-             	jQuery('.newsArchivItem').find('img').each(function() {
-             		if (!(jQuery(this).parent().hasClass('bigThumb'))) {
-             			jQuery(this).remove();
-             		}
-             	});
+		jQuery('.newsArchivItem').find('img').each(function() {
+			if (!(jQuery(this).parent().hasClass('bigThumb'))) {
+				jQuery(this).remove();
+			}
+		});
 
-          	jQuery(window).unbind('scroll').bind('scroll', function() {
-             		if(jQuery(window).scrollTop() + jQuery(window).height() == jQuery(document).height()) {
-             			rowNumber += 1;
-            			getNextArchivData(rowNumber);
-            		}
-          	});	
+		jQuery(window).unbind('scroll').bind('scroll', function() {
+			if(jQuery(window).scrollTop() + jQuery(window).height() == jQuery(document).height()) {
+				rowNumber += 1;
+				getNextArchivData(rowNumber, jQuery('#archiveContentArea', 'archivLazyLoading'));
+			}
+		});	
 	}
 });
 
-function getNextArchivData(rowNumber) {
-	jQuery.get(foswiki.preferences.SCRIPTURL + '/view' + foswiki.preferences.SCRIPTSUFFIX  + '/' + foswiki.preferences.WEB + '/ArchivContentLoad?contenttype=text/plain;skin=text;section=archivLazyLoading;rowNumber=' + rowNumber , function(data) {
-		jQuery('#archiveContentArea').append(data);
+function getNextArchivData(rowNumber, obj, section, callback) {
+	jQuery.get(foswiki.preferences.SCRIPTURL + '/view' + foswiki.preferences.SCRIPTSUFFIX  + '/News/ArchivContentLoad?contenttype=text/plain;skin=text;section=' + section + ';rowNumber=' + rowNumber , function(data) {
+		jQuery(obj).append(data);
   	}).done(function() {
-		jQuery('#archiveContentArea').find('img').each(function() {
+		jQuery(obj).find('img').each(function() {
   			if (!(jQuery(this).parent().hasClass('bigThumb'))) {
   				jQuery(this).remove();
  			}
    		});
+		
+		if (typeof callback != 'undefined') {
+			try {
+				callback();
+			} catch(e) {}
+		}
   	});
 }
+
+function setRightBarConfig() {
+	jQuery('.modacRightBar .newsItem .summary').each(function() {
+		var wholeText   = jQuery(this).html();
+		var partText    = wholeText.substring(0, 300);
+		if (wholeText.length > 300) {
+				jQuery(this).after('<div class="partSummary">' + partText + ' ...</div>').hide();
+		}
+ 	});
+
+    jQuery('.modacRightBar .newsItem .partSummary').unbind().bind('click', function() {
+		jQuery(this).slideUp('normal', function() {
+			jQuery(this).prev().slideDown('normal');
+		});
+  	});
+
+	jQuery('.modacRightBar').find('img').each(function() {
+		if (!(jQuery(this).parent().hasClass('bigThumb')) && !jQuery(this).parent().hasClass('newsTopIconLinks')) {
+			jQuery(this).remove();
+		}
+	});
+}
+
